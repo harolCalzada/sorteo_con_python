0378734a2b1306d92aeb04d3782673838100bfa0
haro28 <harol.calzada@gmail.com>
2016-05-10 00:35:46 -0500

    removiendo algunos prints

--- script_sorteo.py
+++ script_sorteo.py
@@ -2,8 +2,7 @@ import csv
 from random import shuffle, choice
 entries = {}
 with open('/home/staffcreativa-luhego/Desktop/concursos_intersectados.csv', 'rb') as inputfile:
-        reader = csv.reader(inputfile, delimiter=',')
-        print 'reader', reader
+        reader = csv.reader(inputfile, delimiter=',')        
         for row in reader:            
             key=row[0].zfill(8)
             entries[key] = 0
@@ -13,20 +12,17 @@ with open('/home/staffcreativa-luhego/Desktop/Concurso_cantidades.csv', 'rb') as
     contador = 0
     for row in reader2:
         if row[0] in entries.keys():  
-            contador += 1          
-            print 'row',row[0], row[5]
+            contador += 1                   
             entries[row[0]] = entries[row[0]] + int(row[5])
-print 'entries', entries
-print 'contador', contador
+
 
 def ganador():
     lista_sorteo = []
     for key in entries.keys():
         for i in range(entries[key]):
-            lista_sorteo.append(key)
-    print lista_sorteo
-    print 'lista count', len(lista_sorteo)
+            lista_sorteo.append(key)    
     shuffle(lista_sorteo)
     winner = choice(lista_sorteo)
     return winner
+    
 print ganador()
